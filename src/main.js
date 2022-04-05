2fe349e7a950fe6814d63250301c83d36341e33b
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-05 01:03:57 -0400

    add router for vue app

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/theme-chalk/index.css";
 import App from "./App.vue";
+import router from "./router/index.js"
 
-const app = createApp(App).use(ElementPlus);
+const app = createApp(App)
+    .use(ElementPlus)
+    .use(router);
 
 app.use(createPinia());
 
